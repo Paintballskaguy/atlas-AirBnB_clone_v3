3b909326a1d786110b1b311307cec8a2872159fc
SudoSavvy <donovanjheape@gmail.com>
2024-11-08 15:46:43 -0600

    Added entire api folder in project repo as task 4 asks

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
+"""
+This module sets up and runs the Flask API for the AirBnB clone project.
+
+It creates the Flask app, registers blueprints, and sets up teardown 
+methods to manage the application's context.
+"""
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
 
+# Initialize the Flask app
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close the storage session."""
+    """Closes the database session."""
     storage.close()
 
 if __name__ == "__main__":
+    # Run the Flask app with specified or default host and port
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
+"""
+This module provides endpoints related to the API status.
+
+It includes a route to check the status of the API.
+"""
+
 from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    """Returns the status of the API."""
+    """GET /status
+    Returns:
+        JSON response with "status": "OK" to indicate API is running.
+    """
     return jsonify({"status": "OK"})
