245a81ba333ecddba3a8957594e6ccba7e602e8a
SudoSavvy <donovanjheape@gmail.com>
2024-11-09 15:01:40 -0600

    Completeing task 7 by adding and coding states.py as asked

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,11 +3,11 @@ Initializes the views Blueprint for the API
 """
 
 from flask import Blueprint
-from api.v1.views.states import *
 
 # Define the Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 # Import views after defining app_views to avoid circular imports
-from api.v1.views.index import *
-from api.v1.views import states
+from api.v1.views import index  # You can import other views this way
+
+# Do not import states here directly; it will be handled later
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,6 +2,7 @@
 from flask import jsonify, request, abort
 from models import storage
 from models.state import State
+from api.v1.views import app_views  # Import app_views from __init__.py
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
@@ -9,8 +10,9 @@ def get_states():
     states = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(states)
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_states():
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves a specific State object by ID"""
     state = storage.get(State, state_id)
     if not state:
         abort(404)
