458ac2b80c4ea3e3fe6c110e7ab3b48c42da2938
Paintballskaguy <Johnstournament@gmail.com>
2024-11-11 13:06:05 -0600

    pycode on app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,14 +13,17 @@ from models import storage
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception=None):
     """Closes storage on teardown"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error=None):
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     import os
