9d9b0261c68b6a8c1cb7afb5e1024a5a46524f05
Paintballskaguy <Johnstournament@gmail.com>
2024-11-11 13:40:25 -0600

    pycode for states.py; updated test_state.py - not clearing yet

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Module for handling State related routes in the API.
-This module includes routes for retrieving, creating, updating, and deleting State objects.
+This module includes routes for retrieving, creating,
+updating, and deleting State objects.
 """
 
 from flask import jsonify, request, abort
@@ -9,11 +10,13 @@ from models import storage
 from models.state import State
 from api.v1.views import app_views
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects"""
     states = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(states)
+
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
@@ -23,7 +26,10 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route(
+    '/states/<state_id>', methods=['DELETE'], strict_slashes=False
+    )
 def delete_state(state_id):
     """Deletes a State object by ID"""
     state = storage.get(State, state_id)
@@ -32,6 +38,7 @@ def delete_state(state_id):
     storage.delete(state)
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
@@ -46,17 +53,18 @@ def create_state():
     storage.save()
     return jsonify(new_state.to_dict()), 201
 
+
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """Updates an existing State object by ID"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    
+
     data = request.get_json()
     if not data:
         abort(400, description="Not a JSON")
-    
+
     # Ignore keys not allowed to be updated
     ignore_keys = ['id', 'created_at', 'updated_at']
     for key, value in data.items():
