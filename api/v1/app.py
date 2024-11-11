e59875b64c82a1e277a493798bfc20ff5ea884a3
Paintballskaguy <Johnstournament@gmail.com>
2024-11-11 12:41:51 -0600

    removed importing index from app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ Main app module to start Flask for the API
 
 
 from flask import Flask, jsonify, Blueprint
-from api.v1.views.index import app_views, index
+from api.v1.views.index import app_views
 from models import storage
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
