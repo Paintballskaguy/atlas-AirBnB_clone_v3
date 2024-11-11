edecf5e3cd1cb6bb4ddd8374cd9f3a2aee6489d6
Paintballskaguy <Johnstournament@gmail.com>
2024-11-11 14:45:33 -0600

    update test_state.py againe

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -75,7 +75,7 @@ class TestState(unittest.TestCase):
         self.ctx = app.app_context()
         self.ctx.push()
         storage.reload()
-        
+
         for state in storage.all(State).values():
             storage.delete(state)
         storage.save()
@@ -86,16 +86,21 @@ class TestState(unittest.TestCase):
         self.ctx.pop()
 
     def test_create_state_unsupported_media_type(self):
-            """Test POST /api/v1/states with unsupported media type (no Content-Type)"""
-            response = self.client.post('/api/v1/states', data="Invalid JSON format")
-            self.assertEqual(response.status_code, 415, "Expected status code 415 for unsupported media type")
+        """Test POST /api/v1/states with unsupported media type"""
+        response = self.client.post(
+            '/api/v1/states', data="Invalid JSON format"
+            )
+        self.assertEqual(response.status_code, 415, "Expected status code 415")
 
     def test_create_state_invalid_json(self):
         """Test POST /api/v1/states with invalid JSON data"""
         # Set Content-Type header but provide invalid JSON format
         headers = {"Content-Type": "application/json"}
-        response = self.client.post('/api/v1/states', data="Invalid JSON format", headers=headers)
-        self.assertEqual(response.status_code, 400, "Expected status code 400 for invalid JSON")
+        response = self.client.post(
+            '/api/v1/states', data="Invalid JSON format", headers=headers
+            )
+        self.assertEqual(response.status_code, 400, (
+            "Expected status code 400 for invalid JSON"))
 
     def test_create_state_missing_name(self):
         """Test POST /api/v1/states with missing 'name' field in JSON"""
